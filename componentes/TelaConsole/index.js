114d921145eba56768172d0b7152df5628d0f49e
Rick <henriqueesca6@gmail.com>
2022-10-18 09:11:57 -0300

    Projeto xbox

---
+++ componentes/TelaConsole/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function TelaConsole(){
+    return (
+        <View>
+            <Text> Tela Console </Text>
+        </View>
+    )
+}
